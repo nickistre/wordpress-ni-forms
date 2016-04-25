527b0aa13ce3f230a1294302b60aa82e89c3b350
Nicholas Istre <nickistre@gmail.com>
2016-04-25 11:09:04 -0500

    Initial Files

---
+++ sample-form.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Sample Form
+ * Description: A sample plugin showing a customizeable form
+ * Version: 0.1
+ * Author: Nicholas Istre
+ *
+ * Created by IntelliJ IDEA.
+ * User: nick
+ * Date: 4/25/16
+ * Time: 11:00 AM
+ */
