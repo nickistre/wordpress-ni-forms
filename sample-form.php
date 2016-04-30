413fc8cb60af80d31fb88876f71bc8af6aa1e02c
Nicholas Istre <nickistre@gmail.com>
2016-04-30 18:03:34 -0500

    Got basic class with working form code working.

--- sample-form.php
+++ sample-form.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Plugin Name: Sample Form
- * Description: A sample plugin showing a customizeable form
+ * Description: A sample plugin showing a customizeable form, complete with
+ *   adding AJAX form submit and a javascript-based anti-bot honeypot field.
  * Version: 0.1
  * Author: Nicholas Istre
  *
@@ -10,3 +11,41 @@
  * Date: 4/25/16
  * Time: 11:00 AM
  */
+
+class SampleForm {
+    /**
+     * SampleForm constructor.
+     *
+     * Sets up shortcode and any other configuration needed by the plugin
+     */
+    public function __construct()
+    {
+        add_shortcode('sample-form', array($this, 'shortcode'));
+    }
+
+    /**
+     * Processes and sets up the form from the shortcode input
+     *
+     * @param array $atts
+     * @param string $content
+     * @param string $tag
+     *
+     * @return string
+     *
+     * @todo This is a stub that simply returns the form (with provided atts)
+     *   back
+     */
+    public function shortcode($atts, $content, $tag)
+    {
+        // Create attributes for form
+        $atts_string = '';
+        foreach ($atts as $name => $value) {
+            $atts_string = sprintf(' %1$s="%2$s"', htmlentities2($name), htmlentities2($value));
+        }
+
+        $output = sprintf('<form%1$s>%2$s</form>', $atts_string, $content);
+        return $output;
+    }
+}
+
+$sampleForm = new SampleForm();
