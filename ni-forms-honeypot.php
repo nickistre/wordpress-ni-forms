b0049bd5400dd719320ef28547ad8c59ce3973c6
Nicholas Istre <nickistre@gmail.com>
2017-06-06 23:41:14 -0700

    Bumped up plugin versions

--- ni-forms-honeypot.php
+++ ni-forms-honeypot.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: NI Forms - Honeypot
  * Description: Addon for NI Forms system that adds a simple anti-bot honeypot system to forms.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: Nicholas Istre
  * GitHub Plugin URI: https://github.com/nickistre/wordpress-ni-forms
  *
--- ni-forms.php
+++ ni-forms.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: NI Forms
  * Description: A base forms plugin for building AJAX-style submitting forms.
- * Version: 0.4.0
+ * Version: 0.4.1
  * Author: Nicholas Istre
  * GitHub Plugin URI: https://github.com/nickistre/wordpress-ni-forms
  *
